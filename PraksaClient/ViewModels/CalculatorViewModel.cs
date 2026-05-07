3ff0e4f1d36529393a657f56b05661fc40ecee57
Luka Kurtovic <kurtovic0luka@gmail.com>
2026-05-07 14:22:15 +0200

    Verzija kalkulatora koja je debugirana uz pomoc gemini

--- PraksaClient/ViewModels/CalculatorViewModel.cs
+++ PraksaClient/ViewModels/CalculatorViewModel.cs
@@ -10,13 +10,13 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
-namespace PraksaClient.ViewModels 
+namespace PraksaClient.ViewModels
 {
     public class CalculatorViewModel : INotifyPropertyChanged
     {
         private string output;
-        public List<int> a { get; }
-        public List<int> b { get; }
+        public List<List<int>> a { get; }
+        public List<string> b { get; }
         public string op { get; set; }
         public bool jd { get; set; } = true;
 
@@ -24,6 +24,9 @@ namespace PraksaClient.ViewModels
 
         public int prvi { get; set; } = 0;
 
+        public List<int> pomocna { get; set; }
+        public List<int> brojevi { get; set; }
+        public List<int> indeksi { get; set; }
         public string Output
         {
             get { return output; }
@@ -39,15 +42,17 @@ namespace PraksaClient.ViewModels
         public ICommand GenerateCommandp { get; }
         public ICommand GenerateCommandC { get; }
 
-        public CalculatorViewModel() {
-            a = new List<int>();
-            b = new List<int>();
+        public CalculatorViewModel()
+        {
+            a = new List<List<int>>();
+            b = new List<string>();
+            brojevi = new List<int>();
+            pomocna = new List<int>();
 
             GenerateCommandj = new GenericCommand(Generate, CanGenerate);
             GenerateCommand1 = new RelayCommand<object>(Generate1);
             GenerateCommandp = new RelayCommand<object>(Generatep);
             GenerateCommandC = new GenericCommand(GenerateC, CanGenerate);
-
         }
 
         public int list2int(List<int> list)
@@ -62,52 +67,98 @@ namespace PraksaClient.ViewModels
 
         private void Generate()
         {
-            
-            if (!loop)
+            a.Add(new List<int>(pomocna));
+            pomocna.Clear();
+
+            brojevi.Clear(); // Changed: Added clear to prevent list accumulation from previous runs
+            for (int j = 0; j < a.Count(); j++)
             {
-                prvi = list2int(a);
+                brojevi.Add(list2int(a[j]));
             }
-            int drugi = list2int(b);
-            int rez = 0;
-            if (op == "+") { rez = prvi + drugi; }
-            if (op == "*") { rez = prvi * drugi; }
-            if (op == "-") { rez = prvi - drugi; }
-            if (op == "/") { rez = prvi / drugi; }
-            Output = rez.ToString();
-            prvi = rez;
+
+            int i = 0;
+            while (i < b.Count())
+            {
+                if (b[i] == "*")
+                {
+                    int novi = brojevi[i] * brojevi[i + 1];
+                    brojevi[i] = novi; // Changed: Update current index instead of using Insert(i-1)
+                    brojevi.RemoveAt(i + 1);
+                    b.RemoveAt(i);
+                    // Changed: Removed i++ here because the list shifted; checking current index again
+                }
+                else if (b[i] == "/")
+                {
+                    int novi = brojevi[i] / brojevi[i + 1];
+                    brojevi[i] = novi; // Changed: Update current index instead of using Insert(i-1)
+                    brojevi.RemoveAt(i + 1);
+                    b.RemoveAt(i);
+                    // Changed: Removed i++ here because the list shifted; checking current index again
+                }
+                else
+                {
+                    i++; // Changed: Only increment if no operation was performed
+                }
+            }
+
+            i = 0;
+            while (i < b.Count())
+            {
+                if (b[i] == "+")
+                {
+                    int novi = brojevi[i] + brojevi[i + 1];
+                    brojevi[i] = novi; // Changed: Update current index instead of using Insert(i-1)
+                    brojevi.RemoveAt(i + 1);
+                    b.RemoveAt(i);
+                    // Changed: Removed i++ here because the list shifted; checking current index again
+                }
+                else if (b[i] == "-")
+                {
+                    int novi = brojevi[i] - brojevi[i + 1];
+                    brojevi[i] = novi; // Changed: Update current index instead of using Insert(i-1)
+                    brojevi.RemoveAt(i + 1);
+                    b.RemoveAt(i);
+                    // Changed: Removed i++ here because the list shifted; checking current index again
+                }
+                else
+                {
+                    i++; // Changed: Only increment if no operation was performed
+                }
+            }
+
+            if (brojevi.Count > 0)
+            {
+                Output = brojevi[0].ToString();
+            }
+
+            // Changed: Clear state so next calculation starts fresh
             a.Clear();
             b.Clear();
-            loop = true;
         }
+
         private void GenerateC()
         {
-            a.Clear();
             b.Clear();
-           
+            a.Clear(); // Changed: Added clear for 'a'
+            pomocna.Clear(); // Changed: Added clear for 'pomocna'
+            brojevi.Clear(); // Changed: Added clear for 'brojevi'
             Output = "";
-            jd = true;
-
-            loop = false;
         }
 
         private void Generate1(object parameter)
         {
             int broj = int.Parse(parameter.ToString());
             int stringBroj = int.Parse(parameter.ToString());
-            if (jd)
-            {
-                a.Add(broj);
-            }
-            else
-            {
-                b.Add(broj);
-            }
+            pomocna.Add(broj);
             Output += stringBroj;
         }
+
         private void Generatep(object parameter)
         {
             op = parameter.ToString();
-            jd = false;
+            b.Add(op);
+            a.Add(new List<int>(pomocna));
+            pomocna.Clear();
             Output += op;
         }
 
@@ -115,7 +166,6 @@ namespace PraksaClient.ViewModels
         {
             return true;
         }
-
 
         public event PropertyChangedEventHandler PropertyChanged;
 
